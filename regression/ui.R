16da82794005d2f3cf1d942b8b3bbb9d0376dd8d
Ken Kellner <ken@kenkellner.com>
2017-05-18 14:51:13 -0400

    Remove checkbox for now

--- regression/ui.R
+++ regression/ui.R
@@ -13,7 +13,7 @@ fluidPage(
       "and the correlation is:",
       div(style = "color: blue", strong(textOutput("corr_text"))),
       br(),
-      checkboxInput("resids", label = "Plot Residuals?", value = TRUE),
+      #checkboxInput("resids", label = "Plot Residuals?", value = TRUE),
       actionButton("reset_button", "Reset Points")
     ),
     mainPanel(
