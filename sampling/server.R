50e2edfa223377b38cea632ea50628532630d6bb
Ken Kellner <ken@kenkellner.com>
2017-05-19 13:28:29 -0400

    Finish sampling simulator

--- sampling/server.R
+++ sampling/server.R
@@ -4,9 +4,16 @@ function(input,output){
 
   xy <- cbind(rep(1:10,10),rep(1:10,each=10))
 
-  deer.weights <- reactive(rnorm(100,input$wt.mean,input$wt.sd))
+  wts <- reactive(
 
-  deer.sim <- function(sample.size,deer.weights){
+    #return( rnorm(100,input$wt.mean,input$wt.sd))
+    return( rnorm(100,100,20))
+
+  )
+
+  deer.sim <- function(sample.size){
+
+    deer.weights <- wts()
 
     pop.mean <- mean(deer.weights)
     pop.sd <- sd(deer.weights)
@@ -24,24 +31,53 @@ function(input,output){
   }
 
   runsim <- reactive(
-    deer.sim(input$sample.size,deer.weights)
+    deer.sim(input$sample.size)
   )
 
+  hist <- reactiveValues(
+    samp = NULL,
+    mn = NULL
+  )
+
+  observeEvent(input$sample.size,{
+    inp <- runsim()
+    hist$samp <- c(hist$samp,input$sample.size)
+    hist$mn <- c(hist$mn,inp$samp.mean)
+
+  })
+
   output$plot1 <- renderPlot({
+    inp <- runsim()
 
     par(mar=c(1,1,1,1))
 
     plot(xy,pch=19,xlab="",ylab="",xaxt='n',yaxt='n',cex=2)
-    points(xy[runsim$sample.ind,],cex=2,col='red',pch=19)
+    points(xy[inp$sample.ind,],cex=2,col='red',pch=19)
 
+
+  },height=400,width=400)
+
+  output$plot2 <- renderPlot({
+    inp <- runsim()
+
+    sorted <- list()
+    sorted$samp <- hist$samp[order(hist$samp)]
+    sorted$mn <- hist$mn[order(hist$samp)]
+
+    par(mar=c(5,4,1,1))
+    plot(sorted$samp,sorted$mn,col='red',pch=19,cex=2,
+         xlab='Sample Size',ylab="Sample Mean")
+    legend('bottomleft',lty=2,legend='Population Mean')
+    abline(h=inp$pop.mean,lwd=2,lty=2)
 
   })
 
-  output$pop.mean <- renderText({round(runsim$pop.mean,2)})
-  output$pop.sd <- renderText({round(runsim$pop.sd,2)})
 
-  output$samp.mean <- renderText({round(runsim$samp.mean,2)})
-  output$samp.sd <- renderText({round(runsim$samp.sd,2)})
+   output$pop.mean <- renderText({inp <- runsim(); round(inp$pop.mean,2)})
+  # output$pop.sd <- renderText({round(runsim$pop.sd,2)})
+  #
+  output$samp.mean <- renderText({inp <- runsim(); round(inp$samp.mean,2)})
+  output$samp.sd <- renderText({inp <- runsim(); round(inp$samp.sd,2)})
 
 
 
--- sampling/ui.R
+++ sampling/ui.R
@@ -4,23 +4,28 @@ fluidPage(
   titlePanel("Sampling Simulator"),
   sidebarLayout(
     sidebarPanel(
-      sliderInput("wt.mean", label = h3("Population Mean"),
-                  min = 70, max = 150, value = 100),
-      sliderInput("wt.sd", label = h3("Population Mean"),
-                  min = 0, max = 30, value = 20),
-      sliderInput("sample.size", label = h3("Sample Size"),
-                  min = 1, max = 100, value = 10),
+      h4("Population Mean:"),
+      div(style = "color: black; font-size: 30px", strong(textOutput("pop.mean"))),
+      br(),
+      # sliderInput("wt.mean", label = h3("Population Mean"),
+      #             min = 70, max = 150, value = 100),
+      # sliderInput("wt.sd", label = h3("Population SD"),
+      #             min = 0, max = 30, value = 20),
+      sliderInput("sample.size", label = h4("Sample Size"),
+                  min = 0, max = 100, value = 10),
       br(),
 
-      "Sample Mean:",
-      br(),
-      div(style = "color: red", strong(textOutput("samp.mean"))),
-      br(),
-      "Sample SD:",
-      div(style = "color: red", strong(textOutput("samp.sd"))),
+      h4("Sample Mean:"),
+      div(style = "color: red; font-size: 30px", strong(textOutput("samp.mean")))#,
+      # br(),
+      # "Sample SD:",
+      # div(style = "color: red", strong(textOutput("samp.sd")))
     ),
     mainPanel(
+      h4("Sampled Individuals"),
       plotOutput("plot1"),
+      h4("Sample Size vs. Sample Mean"),
+      plotOutput("plot2")
     )
   )
 )
