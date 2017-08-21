b4665a7199cdc4756d3c3404987b8292df75ef92
Ken Kellner <ken@kenkellner.com>
2017-08-20 20:22:57 -0400

    Polish CI app

--- confidence-intervals/server.R
+++ confidence-intervals/server.R
@@ -1,7 +1,5 @@
 library(shiny)
 
-
-
 server = function(input, output) {
   
   samp.hist <- reactiveValues(
@@ -18,8 +16,12 @@ server = function(input, output) {
 
   })
   
-  observeEvent(input$reset, {
-    
+  #List of all conditions that when change
+  #should reset app
+  resetConditions <- reactive({
+    list(input$reset,input$CIlevel,input$samplesize)
+  })
+  observeEvent(resetConditions(),{
     samp.hist$x <- list()
     samp.hist$overlap <- NULL
     
@@ -27,20 +29,26 @@ server = function(input, output) {
   
   output$distplot <- renderPlot({
     
+    par(mar = c(2,4.5,2,2) + 0.1)
+    
     x <- seq(-3,3,by=0.01)
     y <- dnorm(x)
-    plot(x,y,type='l',yaxt='n',ylab='',xlab='Parameter Value',lwd=2)
+    plot(x,y,type='l',yaxt='n',ylab='',xlab='',lwd=2,
+         main="Population Distribution")
     abline(v=0,lty=2)
+    text(0,0.3,expression(italic(μ)),cex=3)
     
   })
   
   output$CIplot <- renderPlot({
     
+    par(mar = c(4,4.5,2,2) + 0.1)
+    
     bound <- function(dat,type){
       mn <- mean(dat)
       se <- 1/sqrt(length(dat))
-      if(type=='lower'){return(mn-1.96*se)}
-      if(type=='upper'){return(mn+1.96*se)}
+      if(type=='lower'){return(mn-as.numeric(input$CIlevel)*se)}
+      if(type=='upper'){return(mn+as.numeric(input$CIlevel)*se)}
     }
     
     
@@ -53,7 +61,8 @@ server = function(input, output) {
       lower <- sapply(samp.hist$x,bound,'lower')
       
       plot(mns,struct,xlim=c(-3,3),yaxt='n',ylab="",
-           xlab="Parameter Value",cex=0.5)
+           main='Statistic Value and CI',
+           xlab="",cex=0.5)
       abline(v=0,lty=2)
       
       cols <- rep('black',length(samp.hist$x))
@@ -66,7 +75,8 @@ server = function(input, output) {
       
     } else{
       plot('n',type='n',xlim=c(-3,3),ylim=c(0,1),
-           yaxt='n',ylab="",xlab="Parameter Value")
+           main='Statistic Value and CI',
+           yaxt='n',ylab="",xlab="")
       abline(v=0,lty=2)
     }
   })
@@ -75,11 +85,24 @@ server = function(input, output) {
   output$overlap <- renderText(paste('Misses:',sum(samp.hist$overlap)))
   output$pct <- renderText(
     if(length(samp.hist$overlap)>0){
-      paste('Overlap: ',round(100-mean(samp.hist$overlap)*100),'%',sep="")
+      paste('Overlap μ: ',round(100-mean(samp.hist$overlap)*100),'%',sep="")
     } else{
-      paste('Overlap:')
+      paste('Overlap μ:')
     }
     
-    )
+  )
+  
+  output$eqnum <- renderUI({
+    withMathJax(
+      sprintf("$$\\mu\\pm%.02f\\cdot\\frac{%.02f}{\\sqrt{%.0f}}$$", 
+              as.numeric(input$CIlevel),1,input$samplesize))
+  })
+  
+  output$eq <- renderUI({
+    withMathJax(
+      sprintf(" $$\\mu\\pm z\\cdot\\frac{\\sigma}{\\sqrt{n}}=$$"))
+  })
+  
+
   
 }
--- confidence-intervals/ui.R
+++ confidence-intervals/ui.R
@@ -4,16 +4,26 @@ fluidPage(
   titlePanel("Confidence Intervals"),
   sidebarLayout(
     sidebarPanel(
+      div(style = "color: black; font-size: 25px", strong("Settings")),
+      radioButtons('CIlevel','Confidence Level',
+                   choiceValues=c(1.645,1.96,2.576),
+                   choiceNames=c('90%','95%','99%'),inline=T,
+                   selected=1.96),
       sliderInput("samplesize", "Sample Size:",
                   min = 1, max = 50, value = 10),
-      numericInput('nsamples', 'Number of Samples to Add:', 1,min = 1, max = 10),
+      sliderInput("nsamples", 'Number of Samples to Add:',
+                  min = 1, max = 10, value = 1),
       actionButton("add", "Add Samples"),
       actionButton("reset", "Reset"),
       br(),
       br(),
-      div(style = "color: black; font-size: 25px", strong(textOutput("count"))),
-      div(style = "color: red; font-size: 25px", strong(textOutput("overlap"))),
-      div(style = "color: black; font-size: 25px", strong(textOutput("pct")))
+      div(style = "color: black; font-size: 25px", strong("Output")),
+      div(style = "color: black; ", strong(textOutput("count"))),
+      div(style = "color: red; ", strong(textOutput("overlap"))),
+      div(style = "color: black; ", strong(textOutput("pct"))),
+      div(style = "color: black; ", strong("Equation:")),
+      uiOutput("eq"),
+      uiOutput("eqnum")
       
     ),
     mainPanel(
